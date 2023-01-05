580bbe4eac70a082957116bc6d9dd6839bc0e30d
Esteban Barac <EstebanBarac8@gmail.com>
2023-01-05 18:14:55 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from '../src/context/ThemeContext';
 import { Navbar } from './components/Navbar';
 import Home from '../src/routes/Home'
@@ -6,14 +6,25 @@ import Singin from '../src/routes/Singin'
 import Singup from '../src/routes/Singup'
 import Account from '../src/routes/Account'
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=true';
+
+  useEffect(()=> {
+    axios.get(url).then((response)=> {
+      setCoins(response.data);
+    })
+  },[url])
+
   return (
     <ThemeProvider >
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home coins={coins} />} />
         <Route path='/signin' element={<Singin />} />
         <Route path='/signup' element={<Singup />} />
         <Route path='/account' element={<Account />} />
